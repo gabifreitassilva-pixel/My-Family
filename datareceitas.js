122a9214db61c8d5c9d69f4da85ed32c877ebc7b
gabifreitassilva-pixel <gabifreitassilva@gmail.com>
2026-02-11 08:08:03 -0300

    Create datareceitas.js

---
+++ datareceitas.js
@@ -0,0 +1,9 @@
+const receitas = [];
+
+for(let i=1;i<=200;i++){
+receitas.push({
+nome:"Receita Fitness "+i,
+ingredientes:"Ingredientes saudáveis variados.",
+modo:"Modo de preparo simples e rápido."
+});
+}
