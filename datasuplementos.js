adec28a1b1d019684b546c34f567c62272052ea6
gabifreitassilva-pixel <gabifreitassilva@gmail.com>
2026-02-11 08:08:26 -0300

    Create datasuplementos.js

---
+++ datasuplementos.js
@@ -0,0 +1,6 @@
+const suplementos = {
+ganho_massa:["Whey","Creatina","Hipercalórico","Beta-Alanina"],
+perda_peso:["Whey Isolado","Cafeína","Chá Verde","L-Carnitina"],
+definicao:["Creatina","BCAA","Termogênico"],
+saude:["Multivitamínico","Ômega 3","Vitamina D"]
+};
