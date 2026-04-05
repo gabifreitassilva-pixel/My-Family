ef3e56cb709a81e086fa86a5be82b45bb3cb44ac
gabifreitassilva-pixel <gabifreitassilva@gmail.com>
2026-04-05 08:30:06 -0300

    Add service worker for caching and offline support

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'mfp-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instala o Service Worker e guarda os ficheiros em cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Interceta os pedidos à rede e usa a cache se estiver offline (sem internet)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Se encontrar na cache, retorna; se não, vai à internet
+        return response || fetch(event.request);
+      })
+  );
+});
