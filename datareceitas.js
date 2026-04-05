778ae356c61daa87a29ca18b933392f9265c562b
gabifreitassilva-pixel <gabifreitassilva@gmail.com>
2026-04-05 08:09:14 -0300

    Delete datareceitas.js

--- datareceitas.js
+++
@@ -1,9 +0,0 @@
-const receitas = [];
-
-for(let i=1;i<=200;i++){
-receitas.push({
-nome:"Receita Fitness "+i,
-ingredientes:"Ingredientes saudáveis variados.",
-modo:"Modo de preparo simples e rápido."
-});
-}
